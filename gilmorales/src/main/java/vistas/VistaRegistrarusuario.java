fb884146f1f65560061954817d687263484d6753
Cristian Gil García <cgg734@inlumine.ual.es>
2023-03-18 14:11:54 +0100

    Nueva subida de iniciar sesion

---
+++ gilmorales/src/main/java/vistas/VistaRegistrarusuario.java
@@ -0,0 +1,27 @@
+package vistas;
+
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.littemplate.LitTemplate;
+import com.vaadin.flow.dom.Element;
+import com.vaadin.flow.component.template.Id;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
+/**
+ * A Designer generated component for the vista-registrarusuario template.
+ *
+ * Designer will add and remove fields with @Id mappings but
+ * does not overwrite or otherwise change this file.
+ */
+@Tag("vista-registrarusuario")
+@JsModule("./src/vista-registrarusuario.ts")
+public class VistaRegistrarusuario extends LitTemplate {
+
+    /**
+     * Creates a new VistaRegistrarusuario.
+     */
+    public VistaRegistrarusuario() {
+        // You can initialise any data required for the connected UI components here.
+    }
+
+}
