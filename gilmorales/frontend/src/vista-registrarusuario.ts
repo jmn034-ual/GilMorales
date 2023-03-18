71113ee0736b4b827772e3a12246ca1502a609ae
Cristian Gil García <cgg734@inlumine.ual.es>
2023-03-18 16:33:27 +0100

    Final registro de usuario. Sigo con iniciarsesion

--- gilmorales/frontend/src/vista-registrarusuario.ts
+++ gilmorales/frontend/src/vista-registrarusuario.ts
@@ -4,6 +4,8 @@ import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
 import '@vaadin/vaadin-button/src/vaadin-button.js';
 import '@polymer/iron-icon/iron-icon.js';
 import '@vaadin/vaadin-text-field/src/vaadin-text-field.js';
+import '@vaadin/vaadin-radio-button/src/vaadin-radio-group.js';
+import '@vaadin/vaadin-radio-button/src/vaadin-radio-button.js';
 
 @customElement('vista-registrarusuario')
 export class VistaRegistrarusuario extends LitElement {
@@ -27,41 +29,75 @@ export class VistaRegistrarusuario extends LitElement {
    <iron-icon icon="lumo:search" slot="prefix"></iron-icon>
   </vaadin-text-field>
   <vaadin-button style="flex-grow: 1; margin: var(--lumo-space-m);">
-   Inicio
+    Inicio 
   </vaadin-button>
   <vaadin-button style="margin: var(--lumo-space-m); flex-grow: 1;">
-   Iniciar sesión
+    Iniciar sesión 
   </vaadin-button>
   <vaadin-button style="margin: var(--lumo-space-m); align-self: flex-start; flex-grow: 1;">
-   Registrarse 
+    Registrarse 
   </vaadin-button>
  </vaadin-horizontal-layout>
  <vaadin-horizontal-layout theme="spacing" style="align-self: center; margin: var(--lumo-space-xl);">
   <label>Registrarse en la web</label>
  </vaadin-horizontal-layout>
- <vaadin-horizontal-layout style="width: 7%; align-self: flex-start; flex-shrink: 0; flex-grow: 0; margin: 1%;">
-  <label style="width: 100%;">Nombre: </label>
- </vaadin-horizontal-layout>
- <vaadin-horizontal-layout style="width: 7%; align-self: flex-start; flex-shrink: 0; flex-grow: 0; margin: 1%;">
-  <label style="width: 100%;">Apellidos: </label>
- </vaadin-horizontal-layout>
- <vaadin-horizontal-layout style="width: 7%; align-self: flex-start; flex-shrink: 0; flex-grow: 0; margin: 1%;">
-  <label style="width: 100%;">Email:</label>
- </vaadin-horizontal-layout>
- <vaadin-horizontal-layout style="width: 9%; align-self: flex-start; flex-shrink: 0; flex-grow: 0; margin: 1%;">
-  <label style="width: 100%;">Contraseña: </label>
- </vaadin-horizontal-layout>
- <vaadin-horizontal-layout style="width: 8%; align-self: flex-start; flex-shrink: 0; flex-grow: 0; margin: 1%;">
-  <label style="width: 100%;">Confirmar:</label>
- </vaadin-horizontal-layout>
- <vaadin-horizontal-layout style="width: 9%; align-self: flex-start; flex-shrink: 0; flex-grow: 0; margin: 1%;">
-  <label style="width: 100%;">Descripción:</label>
- </vaadin-horizontal-layout>
- <vaadin-horizontal-layout style="width: 15%; align-self: flex-start; flex-shrink: 0; flex-grow: 0; margin: 1%;">
-  <label style="width: 100%;">Nombre de usuario: </label>
- </vaadin-horizontal-layout>
- <vaadin-horizontal-layout style="width: 9%; align-self: flex-start; flex-shrink: 0; flex-grow: 0; margin: 1%;">
-  <label style="width: 100%;">Fecha de nacimiento: </label>
+ <vaadin-vertical-layout theme="spacing" style="width: 70%; align-self: center; flex-grow: 0; flex-shrink: 1;">
+  <vaadin-horizontal-layout theme="spacing-xl" style="align-self: center; width: 80%;">
+   <label style="width: 20%; align-self: center;">Nombre:</label>
+   <vaadin-text-field style="width: 100%;"></vaadin-text-field>
+  </vaadin-horizontal-layout>
+  <vaadin-horizontal-layout theme="spacing-xl" style="align-self: center; width: 80%;">
+   <label style="width: 20%; align-self: center;">Apellidos:</label>
+   <vaadin-text-field style="width: 100%;"></vaadin-text-field>
+  </vaadin-horizontal-layout>
+  <vaadin-horizontal-layout theme="spacing-xl" style="align-self: center; width: 80%;">
+   <label style="width: 20%; align-self: center;">Email:</label>
+   <vaadin-text-field style="width: 100%;"></vaadin-text-field>
+  </vaadin-horizontal-layout>
+  <vaadin-horizontal-layout theme="spacing-xl" style="align-self: center; width: 80%;">
+   <label style="width: 20%; align-self: center;">Contraseña:</label>
+   <vaadin-text-field style="width: 100%;"></vaadin-text-field>
+  </vaadin-horizontal-layout>
+  <vaadin-horizontal-layout theme="spacing-xl" style="align-self: center; width: 80%;">
+   <label style="width: 20%; align-self: center;">Confirmar:</label>
+   <vaadin-text-field style="width: 100%;"></vaadin-text-field>
+  </vaadin-horizontal-layout>
+  <vaadin-horizontal-layout theme="spacing-xl" style="align-self: center; width: 80%;">
+   <label style="width: 20%; align-self: center;">Descripción:</label>
+   <vaadin-text-field style="width: 100%;"></vaadin-text-field>
+  </vaadin-horizontal-layout>
+  <vaadin-horizontal-layout theme="spacing-xl" style="align-self: center; width: 80%;">
+   <label style="width: 20%; align-self: center;">Nombre de usuario:</label>
+   <vaadin-text-field style="width: 100%;"></vaadin-text-field>
+  </vaadin-horizontal-layout>
+  <vaadin-horizontal-layout theme="spacing-xl" style="align-self: center; width: 80%;">
+   <label style="width: 15%; align-self: center;">Fecha de naciemiento:</label>
+   <vaadin-text-field style="width: 100%;"></vaadin-text-field>
+  </vaadin-horizontal-layout>
+  <vaadin-horizontal-layout theme="spacing" style="align-self: center; width: 80%;">
+   <label style="width: 75%;">Tipo de usuario:</label>
+   <vaadin-radio-group value="foo" style="width: 100%;">
+    <vaadin-radio-button name="foo" tabindex="-1">
+     Normal
+    </vaadin-radio-button>
+    <vaadin-radio-button name="bar" tabindex="-1">
+     Comercial
+    </vaadin-radio-button>
+   </vaadin-radio-group>
+  </vaadin-horizontal-layout>
+  <vaadin-horizontal-layout theme="spacing" style="align-self: center;">
+   <vaadin-button style="flex-grow: 1; width: 100%;">
+    Subir fotografía
+   </vaadin-button>
+  </vaadin-horizontal-layout>
+ </vaadin-vertical-layout>
+ <vaadin-horizontal-layout theme="spacing-l" style="align-self: center; flex-grow: 0; margin: var(--lumo-space-l); width: 30%;">
+  <vaadin-button style="flex-grow: 1; width: 100%;">
+   Descartar
+  </vaadin-button>
+  <vaadin-button style="width: 100%;">
+   Confirmar
+  </vaadin-button>
  </vaadin-horizontal-layout>
 </vaadin-vertical-layout>
 `;
