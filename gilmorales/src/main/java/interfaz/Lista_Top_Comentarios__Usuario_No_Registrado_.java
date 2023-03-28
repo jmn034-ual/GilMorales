e43532e8b7643ed06f13e895e184aaed5eed8b16
Joaquín Morales Nieto <jmn034@inlumine.ual.es>
2023-03-28 11:12:33 +0200

    Creacion de la rama listas

--- gilmorales/src/main/java/interfaz/Lista_Top_Comentarios__Usuario_No_Registrado_.java
+++ gilmorales/src/main/java/interfaz/Lista_Top_Comentarios__Usuario_No_Registrado_.java
@@ -1,8 +1,10 @@
-//package interfaz;
-//
-//import java.util.Vector;
-//
-//public class Lista_Top_Comentarios__Usuario_No_Registrado_ {
-//	public Ver_publicaci�n__Usuario_No_Registrado_ _ver_publicaci�n__Usuario_No_Registrado_;
-//	public Vector<Lista_Top_Comentarios__Usuario_No_Registrado__item> _item = new Vector<Lista_Top_Comentarios__Usuario_No_Registrado__item>();
-//}
+package interfaz;
+
+import java.util.Vector;
+
+import vistas.VistaListaTopComentariosUsuarioNoRegistrado;
+
+public class Lista_Top_Comentarios__Usuario_No_Registrado_ extends VistaListaTopComentariosUsuarioNoRegistrado{
+	//public Ver_publicaci�n__Usuario_No_Registrado_ _ver_publicaci�n__Usuario_No_Registrado_;
+	//public Vector<Lista_Top_Comentarios__Usuario_No_Registrado__item> _item = new Vector<Lista_Top_Comentarios__Usuario_No_Registrado__item>();
+}
---
+++ gilmorales/src/main/java/vistas/VistaListaTopComentariosUsuarioNoRegistrado.java
@@ -0,0 +1,37 @@
+package vistas;
+
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.littemplate.LitTemplate;
+import com.vaadin.flow.dom.Element;
+import com.vaadin.flow.component.template.Id;
+
+/**
+ * A Designer generated component for the vista-lista-top-comentarios-usuario-no-registrado template.
+ *
+ * Designer will add and remove fields with @Id mappings but
+ * does not overwrite or otherwise change this file.
+ */
+@Tag("vista-lista-top-comentarios-usuario-no-registrado")
+@JsModule("./src/vista-lista-top-comentarios-usuario-no-registrado.ts")
+public class VistaListaTopComentariosUsuarioNoRegistrado extends LitTemplate {
+
+    @Id("listaTopComentarios")
+	private Element listaTopComentarios;
+
+	/**
+     * Creates a new VistaListaTopComentariosUsuarioNoRegistrado.
+     */
+    public VistaListaTopComentariosUsuarioNoRegistrado() {
+        // You can initialise any data required for the connected UI components here.
+    }
+
+	public Element getListaTopComentarios() {
+		return listaTopComentarios;
+	}
+
+	public void setListaTopComentarios(Element listaTopComentarios) {
+		this.listaTopComentarios = listaTopComentarios;
+	}
+
+}
