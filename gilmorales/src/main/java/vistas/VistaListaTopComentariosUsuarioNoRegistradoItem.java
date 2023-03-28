6ab98027cfa5132d03c0f04b04f2f2cc2f975816
Joaquín Morales Nieto <jmn034@inlumine.ual.es>
2023-03-28 12:48:38 +0200

    Vista ver publicacion usuario no registrado

---
+++ gilmorales/src/main/java/vistas/VistaListaTopComentariosUsuarioNoRegistradoItem.java
@@ -0,0 +1,24 @@
+package vistas;
+
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.littemplate.LitTemplate;
+
+/**
+ * A Designer generated component for the vista-lista-top-comentarios-usuario-no-registrado-item template.
+ *
+ * Designer will add and remove fields with @Id mappings but
+ * does not overwrite or otherwise change this file.
+ */
+@Tag("vista-lista-top-comentarios-usuario-no-registrado-item")
+@JsModule("./src/vista-lista-top-comentarios-usuario-no-registrado-item.ts")
+public class VistaListaTopComentariosUsuarioNoRegistradoItem extends LitTemplate {
+
+    /**
+     * Creates a new VistaListaTopComentariosUsuarioNoRegistradoItem.
+     */
+    public VistaListaTopComentariosUsuarioNoRegistradoItem() {
+        // You can initialise any data required for the connected UI components here.
+    }
+
+}
