84e4ab5bdb37f10d95988c1f0ab668f9a9f05e8b
Cristian Gil García <cgg734@inlumine.ual.es>
2023-03-30 17:01:46 +0200

    Creacion rama administrador y su vista principal

---
+++ gilmorales/src/main/java/vistas/VistaAdministradorPrincipal.java
@@ -0,0 +1,24 @@
+package vistas;
+
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.littemplate.LitTemplate;
+
+/**
+ * A Designer generated component for the vista-administrador-principal template.
+ *
+ * Designer will add and remove fields with @Id mappings but
+ * does not overwrite or otherwise change this file.
+ */
+@Tag("vista-administrador-principal")
+@JsModule("./src/vista-administrador-principal.ts")
+public class VistaAdministradorPrincipal extends LitTemplate {
+
+    /**
+     * Creates a new VistaAdministradorPrincipal.
+     */
+    public VistaAdministradorPrincipal() {
+        // You can initialise any data required for the connected UI components here.
+    }
+
+}
