e00a1349abab527aa3eadc3e6d7ef63e88c14d41
Cristian Gil García <cgg734@inlumine.ual.es>
2023-03-30 19:04:25 +0200

    Agregado de elementos java administrador

--- gilmorales/frontend/src/vista-administrador-principal.ts
+++ gilmorales/frontend/src/vista-administrador-principal.ts
@@ -15,20 +15,20 @@ export class VistaAdministradorPrincipal extends LitElement {
 
   render() {
     return html`
-<vaadin-vertical-layout style="width: 100%; height: 100%; align-items: center;">
+<vaadin-vertical-layout style="width: 100%; height: 100%; align-items: center;" id="vaadinVerticalLayout">
  <vaadin-horizontal-layout theme="spacing" id="layoutCabeceraAdmin" style="margin: var(--lumo-space-s); width: 100%;"></vaadin-horizontal-layout>
- <vaadin-horizontal-layout theme="spacing" style="align-self: flex-start; width: 100%; height: 100%; margin: var(--lumo-space-s);">
-  <vaadin-vertical-layout theme="spacing" style="height: 90%; margin: var(--lumo-space-m); width: 30%; flex-grow: 0;">
-   <vaadin-horizontal-layout theme="spacing" style="width: 100%; justify-content: center; height: 8%;">
-    <h2>Top usuarios</h2>
+ <vaadin-horizontal-layout theme="spacing" id="vaadinHorizontalLayout" style="align-self: flex-start; width: 100%; height: 100%; margin: var(--lumo-space-s);">
+  <vaadin-vertical-layout theme="spacing" id="vaadinVerticalLayout1" style="height: 90%; margin: var(--lumo-space-m); width: 30%; flex-grow: 0;">
+   <vaadin-horizontal-layout theme="spacing" id="vaadinHorizontalLayout1" style="width: 100%; justify-content: center; height: 8%;">
+    <h2 id="topUsuariosL">Top usuarios</h2>
    </vaadin-horizontal-layout>
-   <vaadin-vertical-layout theme="spacing" style="width: 90%; margin: var(--lumo-space-xs); align-self: center; height: 35%; flex-grow: 0;"></vaadin-vertical-layout>
-   <vaadin-horizontal-layout theme="spacing" style="align-self: center; width: 100%; justify-content: center;">
-    <h2>Top Hashtags</h2>
+   <vaadin-vertical-layout theme="spacing" id="vaadinVerticalLayout2" style="width: 90%; margin: var(--lumo-space-xs); align-self: center; height: 35%; flex-grow: 0;"></vaadin-vertical-layout>
+   <vaadin-horizontal-layout theme="spacing" id="vaadinHorizontalLayout2" style="align-self: center; width: 100%; justify-content: center;">
+    <h2 id="topHashtagL">Top Hashtags</h2>
    </vaadin-horizontal-layout>
-   <vaadin-vertical-layout theme="spacing" style="align-self: center; width: 90%; flex-grow: 1; height: 35%;"></vaadin-vertical-layout>
+   <vaadin-vertical-layout theme="spacing" id="vaadinVerticalLayout3" style="align-self: center; width: 90%; flex-grow: 1; height: 35%;"></vaadin-vertical-layout>
   </vaadin-vertical-layout>
-  <vaadin-vertical-layout theme="spacing" style="height: 90%; margin: var(--lumo-space-m); flex-grow: 1; width: 60%;"></vaadin-vertical-layout>
+  <vaadin-vertical-layout theme="spacing" id="layoutPublicaciones" style="height: 90%; margin: var(--lumo-space-m); flex-grow: 1; width: 60%;"></vaadin-vertical-layout>
  </vaadin-horizontal-layout>
 </vaadin-vertical-layout>
 `;
