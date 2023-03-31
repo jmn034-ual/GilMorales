2447137ff9cae8cefc40623adaa47ba97a8c5f1b
Cristian Gil García <cgg734@inlumine.ual.es>
2023-03-31 20:48:14 +0200

    ver comentarios comercial

--- gilmorales/frontend/src/vista-ver-comentarios-comercial.ts
+++ gilmorales/frontend/src/vista-ver-comentarios-comercial.ts
@@ -1,4 +1,7 @@
 import { LitElement, html, css, customElement } from 'lit-element';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
+import '@vaadin/vaadin-button/src/vaadin-button.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
 
 @customElement('vista-ver-comentarios-comercial')
 export class VistaVerComentariosComercial extends LitElement {
@@ -12,7 +15,45 @@ export class VistaVerComentariosComercial extends LitElement {
   }
 
   render() {
-    return html``;
+    return html`
+<vaadin-vertical-layout style="width: 100%; height: 100%;" id="vaadinVerticalLayout">
+ <vaadin-horizontal-layout theme="spacing" id="layoutCabecera" style="align-self: stretch;"></vaadin-horizontal-layout>
+ <vaadin-horizontal-layout id="vaadinHorizontalLayout" style="align-self: stretch; flex-grow: 1; margin: var(--lumo-space-xs);">
+  <vaadin-vertical-layout theme="spacing" id="layoutInfo">
+   <vaadin-horizontal-layout theme="spacing" id="layoutpropietario" style="align-self: center;">
+    <vaadin-vertical-layout theme="spacing" id="layoutFotoPerfil"></vaadin-vertical-layout>
+    <vaadin-vertical-layout id="vaadinVerticalLayout1">
+     <vaadin-button theme="tertiary" id="botonNombreUsuario">
+       NombreUsuario 
+     </vaadin-button>
+     <label id="labelGeolocalizacion">Geolocalización</label>
+    </vaadin-vertical-layout>
+   </vaadin-horizontal-layout>
+   <label id="labelDescripcion" style="align-self: stretch; flex-grow: 0; height: 20%;">Descripción de la publicación...</label>
+   <vaadin-vertical-layout theme="spacing" id="layoutEstadisticas" style="align-self: flex-start; flex-grow: 0; justify-content: flex-start; align-items: flex-start;">
+    <vaadin-horizontal-layout theme="spacing" id="layoutMeGustas" style="align-self: stretch;">
+     <label id="numMeGustas" style="align-self: center;">0</label>
+     <vaadin-button id="botonMeGusta">
+       Me Gusta 
+     </vaadin-button>
+     <label id="labelNumMeGustas" style="align-self: center;">Numero Me Gustas</label>
+    </vaadin-horizontal-layout>
+    <vaadin-horizontal-layout theme="spacing" style="align-self: stretch;" id="layoutNumComentarios">
+     <label id="numComentarios" style="align-self: center;">0</label>
+     <label id="labelComentarios">Numero Comentaios</label>
+    </vaadin-horizontal-layout>
+    <vaadin-horizontal-layout theme="spacing" style="align-self: stretch;" id="layoutVisualizaciones">
+     <label id="numVisualizaciones" style="align-self: center;">0</label>
+     <label id="labelVisualizaciones">Numero Visualizaciones</label>
+    </vaadin-horizontal-layout>
+   </vaadin-vertical-layout>
+  </vaadin-vertical-layout>
+  <vaadin-vertical-layout theme="spacing" id="vaadinVerticalLayout2" style="flex-grow: 1;">
+   <vaadin-vertical-layout theme="spacing" id="layoutListaComentarios" style="align-self: stretch; flex-grow: 1;"></vaadin-vertical-layout>
+  </vaadin-vertical-layout>
+ </vaadin-horizontal-layout>
+</vaadin-vertical-layout>
+`;
   }
 
   // Remove this method to render the contents of this view inside Shadow DOM
--- gilmorales/src/main/java/vistas/VistaVerComentariosComercial.java
+++ gilmorales/src/main/java/vistas/VistaVerComentariosComercial.java
@@ -3,6 +3,11 @@ package vistas;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.littemplate.LitTemplate;
+import com.vaadin.flow.dom.Element;
+import com.vaadin.flow.component.template.Id;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Label;
 
 /**
  * A Designer generated component for the vista-ver-comentarios-comercial template.
@@ -14,7 +19,34 @@ import com.vaadin.flow.component.littemplate.LitTemplate;
 @JsModule("./src/vista-ver-comentarios-comercial.ts")
 public class VistaVerComentariosComercial extends LitTemplate {
 
-    /**
+    @Id("vaadinVerticalLayout")
+	private Element vaadinVerticalLayout;
+	@Id("layoutCabecera")
+	private HorizontalLayout layoutCabecera;
+	@Id("vaadinHorizontalLayout")
+	private HorizontalLayout vaadinHorizontalLayout;
+	@Id("layoutInfo")
+	private Element layoutInfo;
+	@Id("layoutpropietario")
+	private HorizontalLayout layoutpropietario;
+	@Id("layoutFotoPerfil")
+	private Element layoutFotoPerfil;
+	@Id("vaadinVerticalLayout1")
+	private Element vaadinVerticalLayout1;
+	@Id("botonNombreUsuario")
+	private Button botonNombreUsuario;
+	@Id("labelGeolocalizacion")
+	private Label labelGeolocalizacion;
+	@Id("labelDescripcion")
+	private Label labelDescripcion;
+	@Id("layoutEstadisticas")
+	private Element layoutEstadisticas;
+	@Id("vaadinVerticalLayout2")
+	private Element vaadinVerticalLayout2;
+	@Id("layoutListaComentarios")
+	private Element layoutListaComentarios;
+
+	/**
      * Creates a new VistaVerComentariosComercial.
      */
     public VistaVerComentariosComercial() {
