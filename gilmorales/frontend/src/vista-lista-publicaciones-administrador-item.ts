010281b3de06cca9d0b7c46c1c09a52d377ece0a
Cristian Gil García <cgg734@inlumine.ual.es>
2023-03-31 20:15:49 +0200

    arreglo

--- gilmorales/frontend/src/vista-lista-publicaciones-administrador-item.ts
+++ gilmorales/frontend/src/vista-lista-publicaciones-administrador-item.ts
@@ -1,14 +1,6 @@
 import { LitElement, html, css, customElement } from 'lit-element';
 import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
-import 'http://localhost:55478/files/transformed/@vaadin/vaadin-ordered-layout/theme/lumo/vaadin-horizontal-layout.js';" draggable="true" vaadin-dnd-layout-item="true" style="align-self: flex-start;
 import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
-import 'http://localhost:55478/files/transformed/@vaadin/vaadin-ordered-layout/theme/lumo/vaadin-horizontal-layout.js';" draggable="true" vaadin-dnd-layout-item="true" style="align-self: flex-start; width: 100%;
-import 'http://localhost:55478/files/transformed/@vaadin/vaadin-ordered-layout/theme/lumo/vaadin-horizontal-layout.js';" draggable="true" vaadin-dnd-layout-item="true" style="align-self: flex-start; width: 100%; align-items: flex-end;
-import 'http://localhost:55478/files/transformed/@vaadin/vaadin-ordered-layout/theme/lumo/vaadin-horizontal-layout.js';" draggable="true" vaadin-dnd-layout-item="true" style="align-self: flex-start; width: 100%; align-items: flex-end; flex-grow: 1;
-import 'http://localhost:55478/files/transformed/@vaadin/vaadin-ordered-layout/theme/lumo/vaadin-horizontal-layout.js';" draggable="true" vaadin-dnd-layout-item="true" style="align-self: flex-start; width: 100%; align-items: flex-end; flex-grow: 1; justify-content: flex-end;
-import 'http://localhost:55478/files/transformed/@vaadin/vaadin-ordered-layout/theme/lumo/vaadin-horizontal-layout.js';" draggable="true" vaadin-dnd-layout-item="true" style="align-self: flex-start; width: 100%; align-items: flex-end; flex-grow: 1; justify-content: center;
-import 'http://localhost:55478/files/transformed/@vaadin/vaadin-ordered-layout/theme/lumo/vaadin-horizontal-layout.js';" draggable="true" vaadin-dnd-layout-item="true" style="align-self: flex-start; width: 100%; align-items: flex-end; flex-grow: 1; justify-content: flex-start;
-import 'http://localhost:55478/files/transformed/@vaadin/vaadin-ordered-layout/theme/lumo/vaadin-horizontal-layout.js';" draggable="true" vaadin-dnd-layout-item="true" style="align-self: flex-start; width: 100%; align-items: center; flex-grow: 1; justify-content: flex-start;
 import '@vaadin/vaadin-button/src/vaadin-button.js';
 
 @customElement('vista-lista-publicaciones-administrador-item')
@@ -24,56 +16,48 @@ export class VistaListaPublicacionesAdministradorItem extends LitElement {
 
   render() {
     return html`
-<vaadin-vertical-layout style="width: 100%; height: 100%; align-items: flex-start;" id="vaadinVerticalLayout">
- <vaadin-horizontal-layout theme="spacing" id="vaadinHorizontalLayout" style="margin: var(--lumo-space-m); width: 90%;">
-  <vaadin-horizontal-layout theme="spacing" id="vaadinHorizontalLayout1" style="flex-grow: 1; width: 15%; align-self: center; justify-content: center;">
-   <div id="fotoPerfil" style="flex-grow: 0;">
+<vaadin-vertical-layout theme="spacing" id="vaadinVerticalLayout" style="width: 100%; height: 100%;">
+ <vaadin-horizontal-layout theme="spacing" id="vaadinHorizontalLayout" style="width: 100%;">
+  <vaadin-horizontal-layout theme="spacing" id="vaadinHorizontalLayout1" style="width: 10%;">
+   <div id="foto" style="width: 100%; height: 100%;">
      Div 
    </div>
   </vaadin-horizontal-layout>
-  <vaadin-vertical-layout theme="spacing" id="vaadinVerticalLayout1">
-   <vaadin-button theme="tertiary" id="nombreDeUsuario" style="nombreDeUsuario">
-    NombreDeUsuario
+  <vaadin-vertical-layout theme="spacing" id="vaadinVerticalLayout1" style="width: 70%;">
+   <vaadin-button theme="tertiary" id="nombre">
+    nombre de usuario
    </vaadin-button>
-   <label id="localizacion" style="align-self: center;">Geolocalización</label>
+   <label id="geolocalizacion" style="align-self: flex-start;">Geolocalizacion</label>
   </vaadin-vertical-layout>
-  <vaadin-horizontal-layout theme="spacing" id="vaadinHorizontalLayout2" style="flex-wrap: wrap; width: 100%; justify-content: flex-end;">
-   <vaadin-button id="eliminarB" style="align-self: center;">
+  <vaadin-horizontal-layout theme="spacing" id="vaadinHorizontalLayout2" style="align-self: center;">
+   <vaadin-button id="eliminarPub">
     Eliminar
    </vaadin-button>
   </vaadin-horizontal-layout>
  </vaadin-horizontal-layout>
- <vaadin-horizontal-layout theme="spacing" id="vaadinHorizontalLayout3" style="width: 100%; height: 20%;">
-  <vaadin-horizontal-layout theme="spacing" id="vaadinHorizontalLayout4" style="justify-content: center; width: 45%;">
-   <div id="video">
+ <vaadin-horizontal-layout theme="spacing" id="vaadinHorizontalLayout3" style="align-self: center; width: 80%;">
+  <vaadin-vertical-layout theme="spacing" id="vaadinVerticalLayout2">
+   <div id="video" style="width: 100%; height: 100%;">
      Div 
    </div>
-   <label id="numeroVisualizaciones">0</label>
-   <label id="visualizacionesL">Visualizaciones </label>
-  </vaadin-horizontal-layout>
-  <vaadin-vertical-layout theme="spacing" id="vaadinVerticalLayout2" style="flex-grow: 1; align-items: center;">
-   <vaadin-horizontal-layout theme="spacing" id="vaadinHorizontalLayout5" style="flex-grow: 0; width: 100%;">
-    <vaadin-horizontal-layout theme="spacing" id="vaadinHorizontalLayout6" style="flex-grow: 0; justify-content: flex-end; align-items: flex-end; width: 28%;">
-     <label id="numeroMeGustas">0</label>
-    </vaadin-horizontal-layout>
-    <vaadin-horizontal-layout theme="spacing" id="vaadinHorizontalLayout7" style="flex-grow: 0; justify-content: flex-end; width: 25%;">
-     <label id="numeroComentarios">0</label>
-    </vaadin-horizontal-layout>
-   </vaadin-horizontal-layout>
-   <vaadin-horizontal-layout theme="spacing" id="vaadinHorizontalLayout8" style="flex-grow: 0; justify-content: center; width: 80%;">
-    <vaadin-horizontal-layout theme="spacing" id="vaadinHorizontalLayout9" style="flex-grow: 1; justify-content: center;">
-     <label id="meGustasL" style="align-self: center;">Me gustas</label>
-    </vaadin-horizontal-layout>
-    <vaadin-horizontal-layout theme="spacing" id="vaadinHorizontalLayout10" style="flex-grow: 1; justify-content: flex-start;">
-     <vaadin-button id="comentariosB">
-      Ver comentarios
-     </vaadin-button>
-    </vaadin-horizontal-layout>
-   </vaadin-horizontal-layout>
-   <vaadin-horizontal-layout theme="spacing" _vaadin_designer_import_href="import 'http://localhost:55478/files/transformed/@vaadin/vaadin-ordered-layout/theme/lumo/vaadin-horizontal-layout.js';" draggable="true" vaadin-dnd-layout-item="true" style="align-self: flex-start; width: 100%; align-items: center; flex-grow: 1; justify-content: flex-start;" id="vaadinHorizontalLayout11">
-    <label id="descripcion">Descripcion</label>
+   <vaadin-horizontal-layout theme="spacing" id="vaadinHorizontalLayout4">
+    <label id="numVis">0</label>
+    <label id="vis">Visualizaciones</label>
    </vaadin-horizontal-layout>
   </vaadin-vertical-layout>
+  <vaadin-vertical-layout theme="spacing" id="vaadinVerticalLayout3" style="align-items: center;">
+   <label id="numG">0</label>
+   <label id="likesL">Me gustas</label>
+  </vaadin-vertical-layout>
+  <vaadin-vertical-layout theme="spacing" id="vaadinVerticalLayout4" style="align-items: center;">
+   <label id="numC">0</label>
+   <vaadin-button id="comentarios">
+    Ver comentarios
+   </vaadin-button>
+  </vaadin-vertical-layout>
+ </vaadin-horizontal-layout>
+ <vaadin-horizontal-layout theme="spacing" id="vaadinHorizontalLayout5" style="align-self: center; width: 60%; justify-content: flex-start;">
+  <label id="descripcion">Descrpcion</label>
  </vaadin-horizontal-layout>
 </vaadin-vertical-layout>
 `;
