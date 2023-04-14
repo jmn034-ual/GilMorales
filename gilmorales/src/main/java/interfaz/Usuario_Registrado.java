40e00a53c7842903cbefde805cd2bf3365479ec8
Cristian Gil García <cgg734@inlumine.ual.es>
2023-04-14 17:32:00 +0200

    UR

--- gilmorales/src/main/java/interfaz/Usuario_Registrado.java
+++ gilmorales/src/main/java/interfaz/Usuario_Registrado.java
@@ -9,18 +9,7 @@ import vistas.VistaUsuarioRegistrado;
 public class Usuario_Registrado extends VistaUsuarioRegistrado{
 //	public iUsuario_Registrado _iUsuario_Registrado;
 	public Lista_publicaciones__Usuario_Registrado_ listaPublicaciones = new Lista_publicaciones__Usuario_Registrado_();
-<<<<<<< HEAD
-	public Cabecera_Usuario_Registrado _cabecera_Usuario_Registrado;
-	
-	public Usuario_Registrado() {
-		this.getStyle().set("width", "100%");
-		this.getStyle().set("height","100%"); 
-	}
-	
-=======
 	public Cabecera_Usuario_Registrado _cabecera_Usuario_Registrado = new Cabecera_Usuario_Registrado();
-
->>>>>>> branch 'prototipado' of git@github.com:jmn034-ual/GilMorales.git
 	public void Lista_publicaciones__Usuario_Registrado_() {
 		this.getListaPublicaciones().as(VerticalLayout.class).add(listaPublicaciones);;
 	}
