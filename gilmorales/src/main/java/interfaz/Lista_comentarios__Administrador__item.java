6a4a205139e69ede547b1436d89ae921a5eb4c2d
Cristian Gil García <cgg734@inlumine.ual.es>
2023-06-21 19:51:52 +0200

    Ver comentarios final no funcional

--- gilmorales/src/main/java/interfaz/Lista_comentarios__Administrador__item.java
+++ gilmorales/src/main/java/interfaz/Lista_comentarios__Administrador__item.java
@@ -4,12 +4,17 @@ import vistas.VistaListaComentariosUsuarioNoRegistradoItem;
 
 public class Lista_comentarios__Administrador__item extends VistaListaComentariosUsuarioNoRegistradoItem{
 //	private button _borrar_comentarioB;
-	public Lista_comentarios__Administrador_ _lista_comentarios__Administrador_;
+	public Lista_comentarios__Administrador_ _lista_comentarios__Administrador_ = new Lista_comentarios__Administrador_();
 	
 	
 	public Lista_comentarios__Administrador__item() {
 		this.getStyle().set("width", "100%");
-		this.getStyle().set("height", "100%");
+    	this.getStyle().set("height", "100%");
+		this.getFotoPerfil().setImage("icons/icon.png");
+		this.getNombreUsuario().setText("NombreUsuario");
+		this.getComentarioL().setText("Este es un comentaio de prueba...");
+		this.getNumeroMeGusta().setText("0");
+		this.getMeGustaL().setText("Me Gustas");
 	}
 
 	public void Borrar_comentario() {
