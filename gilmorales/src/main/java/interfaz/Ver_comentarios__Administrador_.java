9af8fef03f0105972e1210612168b08bcceda469
Cristian Gil García <cgg734@inlumine.ual.es>
2023-06-21 19:19:23 +0200

    nada mas

--- gilmorales/src/main/java/interfaz/Ver_comentarios__Administrador_.java
+++ gilmorales/src/main/java/interfaz/Ver_comentarios__Administrador_.java
@@ -10,9 +10,16 @@ public class Ver_comentarios__Administrador_ extends VistaVerComentarios{
 	public Ver_publicacion__Administrador_ _ver_publicacion__Administrador_;
 	public Lista_comentarios__Administrador_ _lista_comentarios__Administrador_ = new Lista_comentarios__Administrador_();
 	
-	public Ver_comentarios__Administrador_() {
+	public Ver_comentarios__Administrador_(Lista_Publicaciones__Administrador__item item) {
 		this.getStyle().set("width", "100%");
 		this.getStyle().set("height", "100%");
+		this.getBotonComentar().setVisible(false);
+		this.getBotonEliminarAdmi().setVisible(false);
+		this.getBotonMeGusta().setVisible(false);
+		this.getBotonSeguir().setVisible(false);
+		this.getLayoutComentar().setVisible(false);
+		_lista_comentarios__Administrador_.anadirComentario(comentario);
+		Lista_comentarios__Administrador_();
 	}
 
 	public void Lista_comentarios__Administrador_() {
